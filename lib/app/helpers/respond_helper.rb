e8e0686d95a51a6dd6df6ab3c7757b7c26fc3043
xinghu <hupengxing@gmail.com>
2014-01-07 16:35:43 +0800

    Update respond_helper.rb
    change respond-proxy.html id to respond-proxy

--- lib/app/helpers/respond_helper.rb
+++ lib/app/helpers/respond_helper.rb
@@ -4,9 +4,9 @@ module RespondHelper
     tags = [javascript_include_tag(:respond)]
 
     unless ActionController::Base.asset_host.nil?
-      tags << tag('link', {href: asset_path('respond-proxy.html'), id: 'respond-redirect', rel: 'respond-redirect'})
+      tags << tag('link', {href: asset_path('respond-proxy.html'), id: 'respond-proxy', rel: 'respond-redirect'})
       tags << tag('link', {href: image_path('respond.proxy.gif'), id: 'respond-redirect', rel: 'respond-redirect'})
-      tags << javascript_include_tag(javascript_path('respond.proxy.js'))
+      tags << javascript_include_tag('respond.proxy.js')
     end
 
     tags.join("\n").html_safe
